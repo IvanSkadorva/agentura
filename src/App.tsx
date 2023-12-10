5fd931f047eadf227a2852a05c457f36e6cae205
IvanSkadorva <vaniaskadorva@gmail.com>
2023-12-10 16:34:25 +0100

    config eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from './pages/Home.tsx';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Home } from './pages/Home.tsx'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
-function App() {
+function App (): React.JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-export default App;
+export default App

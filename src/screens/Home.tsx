9550cf8175a22d5e616c3aa69593707b40eabd6f
IvanSkadorva <vaniaskadorva@gmail.com>
2023-12-10 16:54:21 +0100

    add app icon for android and ios

---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import * as React from 'react';
+import { type JSX } from 'react';
+
+export function Home(): JSX.Element {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}

99d81b1da3da78d25ac13fdc1040867758c53e55
IvanSkadorva <vaniaskadorva@gmail.com>
2023-12-12 20:03:04 +0100

    add i18n

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,28 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import React from 'react';
 import MainSvg from '../assets/main.svg';
+import { useTranslation } from 'react-i18next';
 
 export function Home(): JSX.Element {
+  const { t } = useTranslation();
+
   return (
     <View className="container h-full">
       <View className="flex-1 justify-center items-center">
-        <MainSvg height={45} width={45} />
-        <Text>Open up App.npm jff start working on your app!</Text>
+        <MainSvg height={450} width={450} />
+        <Text>{t('gameTitle')}</Text>
+        <Button
+          title={t('buttons.play')}
+          onPress={() => {
+            console.log('Hello');
+          }}
+        />
+        <Button
+          title={t('buttons.howToPlay')}
+          onPress={() => {
+            console.log('Hello');
+          }}
+        />
       </View>
       <View className="h-20 justify-center items-center">
         <Text>Footer</Text>

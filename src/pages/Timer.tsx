83df5dbcc1f17626ac798620747e1bbbabbd1af3
IvanSkadorva <vaniaskadorva@gmail.com>
2024-01-16 09:00:47 +0100

    Add overlay icons

--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -6,9 +6,13 @@ import { TouchableOpacity, View } from 'react-native';
 import { Container } from '../components/atoms/Container.tsx';
 import { BaseText } from '../components/atoms/BaseText.tsx';
 import Clock from '../assets/images/clock.svg';
+import CivilSide from '../assets/images/civil-side.svg';
+import SpySide from '../assets/images/spy-side.svg';
+
 import { ms, mvs, ScaledSheet } from 'react-native-size-matters';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useAppStore } from '../store/app-store.ts';
+import { CORAL_RED } from '../styles/colors.ts';
 
 type TimerProps = NativeStackScreenProps<RootStackParamList, 'Timer'>;
 
@@ -17,28 +21,26 @@ export function Timer(): JSX.Element {
   const { navigate } = useNavigation<TimerProps['navigation']>();
   const defaultTimeInSeconds = useAppStore.use.gameTimeInMinutes() * 60;
   const [time, setTime] = useState(defaultTimeInSeconds);
-  const [isCountdownActive, setIsCountdownActive] = useState(false);
-  let intervalId: NodeJS.Timeout;
+  const intervalRef = useRef<NodeJS.Timeout>();
 
   const toggleCountdown = (): void => {
     console.log(time);
 
-    if (isCountdownActive) {
-      clearInterval(intervalId);
-      setIsCountdownActive(false);
+    if (intervalRef.current !== undefined) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = undefined;
     } else {
-      intervalId = setInterval(() => {
+      intervalRef.current = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
       }, 1000);
-      setIsCountdownActive(true);
     }
   };
 
-  // useEffect(() => {
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, []);
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
 
   return (
     <Container style={styles.wrapper}>
@@ -46,7 +48,7 @@ export function Timer(): JSX.Element {
         <BaseText>{t('hint.timeForQuestions').toUpperCase()}</BaseText>
       </View>
       <TouchableOpacity style={styles.clockContainer} onPress={toggleCountdown}>
-        <BaseText style={styles.clockTimer}>
+        <BaseText style={styles.clockTime}>
           {new Date(time * 1000).toISOString().slice(14, 19)}
         </BaseText>
         <Clock width={ms(313)} height={mvs(313)} />
@@ -54,11 +56,30 @@ export function Timer(): JSX.Element {
       <View style={styles.textContainer}>
         <BaseText>{t('timer.chooseThePerson')}</BaseText>
       </View>
+      <View style={styles.overlayContainer}>
+        <View style={styles.overlaySide}>
+          <CivilSide width={ms(300)} height={mvs(290)} />
+          <BaseText whiteText style={styles.overlayText}>
+            {t('timer.spies')}
+          </BaseText>
+        </View>
+        <BaseText>{t('timer.whoWon')}</BaseText>
+        <View style={styles.overlaySide}>
+          <SpySide width={ms(300)} height={mvs(300)} />
+          <BaseText whiteText style={styles.overlayText}>
+            {t('timer.civils')}
+          </BaseText>
+        </View>
+      </View>
     </Container>
   );
 }
 
 const styles = ScaledSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
   wrapper: {
     display: 'flex',
     flexDirection: 'column',
@@ -74,8 +95,32 @@ const styles = ScaledSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  clockTimer: {
+  clockTime: {
     position: 'absolute',
+    fontSize: '51@ms',
+    lineHeight: '51@ms',
+    fontWeight: '400',
+    color: CORAL_RED,
+    paddingTop: '40@mvs',
     zIndex: 1,
+  },
+  overlayContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    zIndex: 1,
+  },
+  overlaySide: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  overlayText: {
+    position: 'absolute',
+    paddingTop: '80@mvs',
   },
 });

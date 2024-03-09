cc387c525accf34bcae01e566780ada6bcfd731e
IvanSkadorva <vaniaskadorva@gmail.com>
2024-03-09 10:56:00 +0100

    Change android bundle name and edit translations

--- android/app/src/main/java/com/agenutraspygame/MainActivity.kt
+++ android/app/src/main/java/com/agenutraspygame/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.agentura
+package com.agenutraspygame
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/agenutraspygame/MainApplication.kt
+++ android/app/src/main/java/com/agenutraspygame/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.agentura
+package com.agenutraspygame
 
 import android.app.Application
 import com.facebook.react.PackageList
